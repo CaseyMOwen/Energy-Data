4a94c817285cd590b3b87939ffb02ec76a82a759
Casey Owen <casey.michael.owen@gmail.com>
2023-05-09 17:08:49 -0400

    created API outline

--- API.py
+++ API.py
@@ -1,5 +1,26 @@
 import requests
 import config
 
-baseURL =  "https://api.eia.gov/API_route"
-finalURL = baseURL + config.APIKey
+baseURL =  "https://api.eia.gov/v2/"
+route = "electricity/electric-power-operational-data"
+keyURL = "?api_key=" + config.APIKey
+finalURL = baseURL + route + keyURL
+
+headers = {
+    "frequency" : "quarterly",
+    "data" : "consumption-for-eg-btu",
+    "facets" : "",
+    "start" : "2022-Q1",
+    "end" : "null",
+    "sort": [
+        {
+            "column": "period",
+            "direction": "desc"
+        }
+    ],
+    "offset": 0,
+    "length": 5000
+    
+}
+
+response = requests.request("GET", finalURL, headers = headers)
